7537b58b3aa624ebf9218704eb3ddf9f4807fc11
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-08-28 19:01:40 +0700

    Create Get Employee By Id

--- src/main/java/com/tirwanda/react/spring/controller/EmployeeController.java
+++ src/main/java/com/tirwanda/react/spring/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.tirwanda.react.spring.controller;
 
 import com.tirwanda.react.spring.entity.Employee;
+import com.tirwanda.react.spring.exception.ResourceNotFoundException;
 import com.tirwanda.react.spring.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +27,16 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    public Employee createEmployee(@RequestBody Employee empolyee) {
-        return employeeRepository.save(empolyee);
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Employee Not Exist with id : " + id));
+
+        return ResponseEntity.ok(employee);
+    }
+
 }
